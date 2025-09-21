ecf9b5e40c42b42b00bfd8b6f5b804591136b477
Skanda M V <skandagowda0@gmail.com>
2025-09-21 20:52:13 +0530

    Demo App Development

--- demoWebApp/demoWebApp/src/main/java/com/prapthi/demoWebApp/DemoWebAppApplication.java
+++ demoWebApp/demoWebApp/src/main/java/com/prapthi/demoWebApp/DemoWebAppApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoWebAppApplication {
 
 	public static void main(String[] args) {
+        System.out.println("Spring Boot");
 		SpringApplication.run(DemoWebAppApplication.class, args);
-
         System.out.println("SpringBoot");
 	}
 
