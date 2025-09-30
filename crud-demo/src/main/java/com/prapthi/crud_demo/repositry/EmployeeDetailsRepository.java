56b0711e9533494aad7b5e3bf897816fc9c41b89
Skanda M V <skandagowda0@gmail.com>
2025-09-30 10:34:28 +0530

    Basic CRUD Operation And Soft Delete operation

--- crud-demo/src/main/java/com/prapthi/crud_demo/repositry/EmployeeDetailsRepository.java
+++ crud-demo/src/main/java/com/prapthi/crud_demo/repositry/EmployeeDetailsRepository.java
@@ -1,11 +1,19 @@
 package com.prapthi.crud_demo.repositry;
 
+import com.prapthi.crud_demo.dto.EmployeeWithCrudDto;
 import com.prapthi.crud_demo.entity.EmployeeDetailsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeDetailsRepository extends JpaRepository<EmployeeDetailsEntity,Integer> {
+
+
+    @Query("select new com.prapthi.crud_demo.dto.EmployeeWithCrudDto(e.empId, e.)")
+    List<EmployeeWithCrudDto> fetchEmployeeWithCrudDto();
 
 
 }
